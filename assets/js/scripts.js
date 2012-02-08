1f4522dc2201b390ede05275826bf06542c75548
Darcy Clarke <darcy@darcyclarke.me>
2012-02-07 22:23:14 -0700

    Added initial repo folders and files

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+/**
+ * Application Architecture
+ *
+ * @author YourNameHere
+ * @version 1.0
+ *
+ * @param {Object} window, undefined
+ *
+ */
+
+(function(window, undefined){
+    
+
+})(window);
